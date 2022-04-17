c925e456464aef71e55d909d6117146f7b635347
Deniz Ariyan <denizariyan@gmail.com>
2022-04-17 22:29:04 +0300

    Add comments

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const scan = require("./scripts/scan");
 
+// Wait for something here such as "fs.watch" watching for changes on env file and stop and start the scanner process again.
 scan("start");
 //scan("stop");
