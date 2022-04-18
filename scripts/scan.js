cdc33bc5920cb3da1e4e486a05affbf8d8ce2e2a
Deniz Ariyan <denizariyan@gmail.com>
2022-04-18 12:14:21 +0300

    Remove unnecessary configurables

--- scripts/scan.js
+++ scripts/scan.js
@@ -4,37 +4,36 @@ const axios = require('axios');
 const child_process = require('child_process');
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
-let dotenv = require('dotenv');
+require('dotenv').config();
 
-dotenv.config({ override: true });
 options =
 {
     removeInfected: process.env.removeInfected, // If true, removes infected files
     quarantineInfected: process.env.quarantineInfected, // False: Don't quarantine, Path: Moves files to this place.
-    scanLog: process.env.scanLog, // Path to a writeable log file to write scan results into
-    debugMode: process.env.debugMode, // Whether or not to log info/debug/error msgs to the console
-    fileList: process.env.fileList, // path to file containing list of files to scan (for scanFiles method)
-    scanRecursively: process.env.scanRecursively, // If true, deep scan folders recursively
+    scanLog: '/usr/VSterilizer/log/scan.log', // Path to a writeable log file to write scan results into
+    debugMode: true, // Whether or not to log info/debug/error msgs to the console
+    fileList: null, // path to file containing list of files to scan (for scanFiles method)
+    scanRecursively: true, // If true, deep scan folders recursively
     clamscan: {
-        path: process.env.clamscan_path, // Path to clamscan binary on your server
-        db: process.env.clamscan_db, // Path to a custom virus definition database
-        scanArchives: process.env.clamscan_scanArchives, // If true, scan archives (ex. zip, rar, tar, dmg, iso, etc...)
-        active: process.env.clamscan_active // If true, this module will consider using the clamscan binary
+        path: '/usr/bin/clamscan', // Path to clamscan binary on your server
+        db: null, // Path to a custom virus definition database
+        scanArchives: true, // If true, scan archives (ex. zip, rar, tar, dmg, iso, etc...)
+        active: false // If true, this module will consider using the clamscan binary
     },
     clamdscan: {
-        socket: process.env.clamdscan_socket, // Socket file for connecting via TCP
-        host: process.env.clamdscan_host, // IP of host to connect to TCP interface
-        port: process.env.clamdscan_port, // Port of host to use when connecting via TCP interface
-        timeout: process.env.clamdscan_timeout, // Timeout for scanning files
-        localFallback: process.env.clamdscan_localFallback, // Use local preferred binary to scan if socket/tcp fails
-        path: process.env.clamdscan_path, // Path to the clamdscan binary on your server
-        configFile: process.env.clamdscan_configFile, // Specify config file if it's in an unusual place
-        multiscan: process.env.clamdscan_multiscan, // Scan using all available cores! Yay!
-        reloadDb: process.env.clamdscan_reloadDb, // If true, will re-load the DB on every call (slow)
-        active: process.env.clamdscan_active, // If true, this module will consider using the clamdscan binary
-        bypassTest: process.env.clamdscan_bypassTest, // Check to see if socket is available when applicable
+        socket: false, // Socket file for connecting via TCP
+        host: false, // IP of host to connect to TCP interface
+        port: false, // Port of host to use when connecting via TCP interface
+        timeout: 60000, // Timeout for scanning files
+        localFallback: true, // Use local preferred binary to scan if socket/tcp fails
+        path: '/usr/bin/clamdscan', // Path to the clamdscan binary on your server
+        configFile: null, // Specify config file if it's in an unusual place
+        multiscan: true, // Scan using all available cores! Yay!
+        reloadDb: false, // If true, will re-load the DB on every call (slow)
+        active: true, // If true, this module will consider using the clamdscan binary
+        bypassTest: false, // Check to see if socket is available when applicable
     },
-    preference: process.env.preference // If clamdscan is found and active, it will be used by default
+    preference: 'clamdscan' // If clamdscan is found and active, it will be used by default
 }
 
 // Send every bad file one by one
@@ -64,7 +63,7 @@ async function scanDirectory(path) {
     const clamscan = await new NodeClam().init(options);
     try {
         // TODO: Replace hard-coded path with path var it is here for faster testing
-        await clamscan.scanDir(path, async function (err, goodFiles, badFiles, viruses) {
+        clamscan.scanDir(path, async function (err, goodFiles, badFiles, viruses) {
             if (badFiles.length > 0) {
                 let badFileList = await parseLog(path);
                 sendResults(badFileList);
@@ -133,7 +132,7 @@ function start() {
     console.log("Started to monitor for USB inserts!");
     USBWatch.startMonitoring();
     //getMountPoint("C03FD5F2F334F15109A501FD"); // For testing
-    //scanDirectory("/home/deari/Downloads"); // Enable for testing
+    //scanDirectory("/home/deari/Downloads/"); // Enable for testing
 }
 
 function endWatch() {
