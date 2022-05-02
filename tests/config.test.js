30fae48089ba2bc448d2d40638f35d9f1ca372a0
Deniz Ariyan <denizariyan@gmail.com>
2022-05-02 14:47:44 +0300

    Add tests for API listener connectivity check

--- tests/config.test.js
+++ tests/config.test.js
@@ -1,4 +1,5 @@
 const config = require('../scripts/config');
+const { tcpPingPort } = require("tcp-ping-port")
 
 test('Enable quarantine mode', async () => {
     let testReturn = await config.changeParam("quarantineInfected", "/usr/VSterilizer/infected/", "./tests/sources/.test.env")
@@ -8,6 +9,13 @@ test('Enable quarantine mode', async () => {
 test('Enable remove mode', async () => {
     let testReturn = await config.changeParam("removeInfected", "True", "./tests/sources/.test.env")
     expect(testReturn).toBe("removeInfected mode enabled");
+});
+
+test('Check server reachability', async () => {
+    let res = await tcpPingPort("0.0.0.0", 8080).then(status => {
+        return status.online;
+    })
+    expect(res).toBe(true);
 });
 
 afterAll(() => {
